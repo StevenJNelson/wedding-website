22e599237877bf9e9153d33f283635b0e0469649
Steven Nelson <snelson@gomodus.com>
2023-09-18 22:17:39 -0500

    Added basic homescreen w/ tailwind styling & navbar

---
+++ wedding-website/src/App.vue
@@ -0,0 +1,19 @@
+<script setup>
+import Header from './components/Header.vue';
+</script>
+
+<template>
+  <div>
+    <Header />
+    <div class="flex justify-center items-center h-screen">
+        <span class="text-4xl font-bold">Coming soon...</span>
+    </div>
+  </div>
+</template>
+
+<style scoped>
+  coming-soon {
+    display: flex;
+  }
+</style>
+
